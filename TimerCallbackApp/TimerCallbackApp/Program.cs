21566b3afb7f58090f065b3ec90cd771fae065fe
Lionlins <149457524+Lionlins@users.noreply.github.com>
2023-11-06 21:45:49 -0500

    TimerCallback

---
+++ TimerCallbackApp/TimerCallbackApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static Timer _timer;
+
+    static void Main()
+    {
+        _timer = new Timer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+        Console.WriteLine("Timer started. Press Enter to exit.");
+        Console.ReadLine();
+
+        _timer.Dispose();
+    }
+
+    static void TimerCallback(object state)
+    {
+        Console.WriteLine("Timer callback executed at: " + DateTime.Now);
+    }
+}
